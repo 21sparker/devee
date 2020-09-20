c5fbbfac3d05544b6527b09cdff3ae56d721ff88
Sean <seanparker500@gmail.com>
2020-09-20 19:43:42 -0400

    First commit

--- api/api.py
+++ api/api.py
@@ -1,9 +1,40 @@
 import time
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+items = [
+    {
+        "id": "t1",
+        "status": "Not Started",
+        "title": "A task not started",
+        "description": "The description of said task"
+    },
+        {
+        "id": "t2",
+        "status": "Not Started",
+        "title": "A task not really  started",
+        "description": "The description of said task"
+    }
+]
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+
+@app.route('/api/items')
+def get_all_items():
+    """
+    Methods:
+    - GET: Gets all the items in the database.
+    - POST: Adds new item to database.
+    """
+    if request.method == 'POST':
+        pass
+
+    return {'items': items}
+
+
+@app.route('/api/items/<int:item_id>', methods=['GET', 'PUT', 'DELETE'])
+def edit_item(item_id):
+    """
+    Get, edit, or delete an item.
+    """
+    pass
--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: null,
+            currentGroupMethod: "status"
+        };
+
+        this.groupMethods = {
+            "status": ["Not Started", "In Progress", "Complete"]
+        }
+    }
+
+    componentWillMount() {
+        this.renderMyData();
+    }
+
+    renderMyData(){
+        fetch('/api/items')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson["items"])
+                this.setState({ data: responseJson["items"] })
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    groupData() {
+        const columnTitles = this.groupMethods[this.state.currentGroupMethod];
+
+        const columns = [];
+        columnTitles.forEach((columnTitle) => {
+            const data = this.state.data.filter((item) => item[this.state.currentGroupMethod] === columnTitle);
+
+            columns.push({
+                "title": columnTitle,
+                "items": data
+            })
+        });
+
+        return columns;
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.data ? <KanbanBoard columns={this.groupData()} /> : <p>Loading data....</p>}
+            </div>
+        );
+    }
 }
 
-export default App;
+
+
+class KanbanBoard extends Component {
+
+    render() {
+        const columns = this.props.columns.map((col) =>
+            <Column key={col.title} title={col.title} items={col.items} />
+        );
+
+        return (
+            <div>
+                <ul style={{display: "inline-flex", listStyleType: "none"}}>
+                    {columns}
+                </ul>
+            </div>
+        );
+    }
+}
+
+
+class Column extends Component {
+    render() {
+        const cards = this.props.items.map((item) => 
+            <Card key={item.id} item={item} />
+        );
+
+        return (
+            <li>
+                <h3>{this.props.title}</h3>
+                <ul style={{listStyleType: "none"}}>{cards}</ul>
+            </li>
+        )
+    }
+}
+
+class Card extends Component {
+    render() {
+        const item = this.props.item;
+
+        return (
+            <li>
+                <p><b>{item.title}</b> {item.description}</p>
+            </li>
+        )
+    }
+}
