b9b5ab70bda9c7151fb472aed771d00e9b9c6661
Sean <seanparker500@gmail.com>
2020-10-11 12:09:56 -0400

    Add Navbar skeleton Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, {Component} from 'react';
 import './App.css'
 import KanbanBoard from './components/KanbanBoard/KanbanBoard';
+import Navbar from './components/Navbar/Navbar';
 
 
 export default class App extends Component {
-
     render() {
         return (
             <div className="app-container">
-                {/* <Navbar/>
-                <SidePanel/> */}
-                <KanbanBoard/>
+                <Navbar />
+                <KanbanBoard />
             </div>
         );
     }
 }
-
-
